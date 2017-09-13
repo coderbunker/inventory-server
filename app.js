0378f12dd3610bd8d4ba2964df7d3743e94e26e4
Alex Moran <mattermoran@gmail.com>
2017-09-13 18:36:20 +0800

    little changes

--- app.js
+++ app.js
@@ -1,88 +1,81 @@
-var express = require('express');
-var Session = require('express-session');
-var google = require('googleapis');
+const express = require('express');
+const google = require('googleapis');
 const uuid = require('uuid-regexp');
 const urlRegex = require('url-regex');
 
 const keys = require('./config/keys');
 
-var app = express();
+const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
-
-app.get('/favicon.ico', function(req, res) {
-    res.status(204);
+app.get('/favicon.ico', (req, res) => {
+  res.status(204);
 });
 
+app.get('/:id', (req, res) => {
+  const sheets = google.sheets('v4');
+  sheets.spreadsheets.values.get({
+    auth: keys.apiKey,
+    spreadsheetId: '1QHKa3vUpht7zRl_LEzl3BlUbolz3ZiL8yKHzdBL42dY',
+    range: 'Agora inventory!A:Z',
+  }, (err, response) => {
+    if (err) {
+      console.log(`The API returned an error: ${err}`);
+      return;
+    }
+    const rows = response.values;
+    const result = [];
 
-let id = [];
-
-app.get("/:id", function(req, res) {
-
-    //SPREADSHEET
-    var sheets = google.sheets('v4');
-    sheets.spreadsheets.values.get({
-      auth: keys.apiKey,
-      spreadsheetId: '1QHKa3vUpht7zRl_LEzl3BlUbolz3ZiL8yKHzdBL42dY',
-      range: 'Agora inventory!A:Z'
-    }, function(err, response) {
-      if (err) {
-        console.log('The API returned an error: ' + err);
-        return;
-      }
-      var rows = response.values;
-      var result = [];
-      if (rows.length == 0) {
-        console.log('No data found.');
-      } else {
-        for (var i = 0; i < rows.length; i++) {
-          var row = rows[i];
-          if (row[11]) {
-            result.push(row);
-
-          }
+    if (rows.length === 0) {
+      console.log('No data found.');
+    } else {
+      const uuidIndex = rows[0].indexOf('uuid');
+      for (let i = 0; i < rows.length; i += 1) {
+        const row = rows[i];
+        // console.log(row);
+        if (row[uuidIndex]) {
+          result.push(row);
         }
-        let matchedItem = result.find(function(item) {
-          if (uuid().test(item)) {
-            return uuid().exec(item)[0] === req.params.id;
-          }
-        });
-        let list = '';
+      }
+      const matchedItem = result.find((item) => {
+        if (uuid().test(item)) {
+          return uuid().exec(item)[0] === req.params.id;
+        }
+      });
 
-        if (matchedItem) {
-          for (var i = 0; i < 12; i++) {
-            if (matchedItem[i].length > 1) {
-              let text = matchedItem[i];
-              let newUrl;
-              if (urlRegex().test(text)) {
-                newUrl = text.match(urlRegex());
-                newUrl.forEach((url) => {
-                  text = text.replace(url, `<a href="${url}">${url}</a>`)
-                });
-              }
-              list += `<li>${result[0][i].toUpperCase()}: <span class="detail">${text}</span></li>`
+      let listItem = '';
+      if (matchedItem) {
+        for (let i = 0; i < rows[0].length; i += 1) {
+          if (matchedItem[i].length > 1) {
+            let text = matchedItem[i];
+            const newUrl = text.match(urlRegex());
+            if (newUrl) {
+              newUrl.forEach((url) => {
+                text = text.replace(url, `<a href="${url}">${url}</a>`);
+              });
             }
-
+            listItem += `<li>${result[0][i].toUpperCase()}: <span class="detail">${text}</span></li>`;
           }
-          res.render('item', {item: list});
-        } else {
-          res.render('notFound', {item: list, id: req.params.id});
         }
+        res.render('item', { item: listItem });
+      } else {
+        res.render('notFound', {
+          item: listItem,
+          id: req.params.id,
+        });
       }
-    });
+    }
+  });
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello;)');
+  res.redirect('https://cryptic-woodland-88390.herokuapp.com/');
 });
 
-
 const port = process.env.PORT || 1234;
-
-
 
 app.listen(port, () => {
   console.log(`working on ${port}`);
