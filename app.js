1cae42b5395ae8d14ddff71498451e46b53ca168
Spazcool <douglas.wrightiii@gmail.com>
2017-10-14 17:51:57 +0800

    REFACTORED: extract module for google spreadsheet model

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express');
-const google = require('googleapis');
-const uuid = require('uuid-regexp');
 const urlRegex = require('url-regex');
-
-const keys = require('./config/keys');
+const googleSpreadsheet = require('./googleSpreadsheet');
 
 const app = express();
 
@@ -16,73 +13,40 @@ app.get('/favicon.ico', (req, res) => {
 });
 
 function makeClickable(text) {
-  let newText = text;
-  const newUrl = newText.match(urlRegex());
-  if (newUrl) {
-    newUrl.forEach((url) => {
-      newText = text.replace(url, `<a href="${url}">${url}</a>`);
-    });
+  const newUrl = text.match(urlRegex());
+  if (!newUrl) {
+    return text;
   }
+
+  let newText = text;
+  newUrl.forEach((url) => {
+    newText = text.replace(url, `<a href="${url}">${url}</a>`);
+  });
   return newText;
 }
 
-function createRenderObj(val, lab) {
-  const renderObj = {
-    label: lab,
-    value: val,
-  };
-  return renderObj;
-}
 
-function renderTemplate(obj) {
+function renderTemplate(o) {
   let listItem = '';
-  for (let i = 0; i < obj.label.length; i += 1) {
-    if (obj.value[i] && (obj.value[i].length > 1)) {
-      listItem += `<li>${obj.label[i].toUpperCase()}: <span class="detail">${makeClickable(obj.value[i])}</span></li>`;
+  for (const k in o) {
+    if (o[k]) {
+      listItem += `<li>${k.toUpperCase()}: <span class="detail">${makeClickable(o[k])}</span></li>`;
     }
   }
   return listItem;
 }
 
+
 app.get('/:id', (req, res) => {
-  const sheets = google.sheets('v4');
-  sheets.spreadsheets.values.get({
-    auth: keys.apiKey,
-    spreadsheetId: '1QHKa3vUpht7zRl_LEzl3BlUbolz3ZiL8yKHzdBL42dY',
-    range: 'Agora inventory!A:Z',
-  }, (err, response) => {
-    if (err) {
-      console.log(`The API returned an error: ${err}`);
+  googleSpreadsheet.findEquipment(req.params.id, (matchedItem) => {
+    if (!matchedItem) {
+      res.render('notFound', {
+        ite1m: '',
+        id: req.params.id,
+      });
       return;
     }
-    const rows = response.values;
-    const result = [];
-
-    if (rows.length === 0) {
-      console.log('No data found.');
-    } else {
-      const uuidIndex = rows[0].indexOf('uuid');
-      for (let i = 0; i < rows.length; i += 1) {
-        const row = rows[i];
-        // console.log(row);
-        if (row[uuidIndex]) {
-          result.push(row);
-        }
-      }
-      const matchedItem = result.find((item) => {
-        if (uuid().test(item)) {
-          return uuid().exec(item)[0] === req.params.id;
-        }
-      });
-      if (matchedItem) {
-        res.render('item', { item: renderTemplate(createRenderObj(matchedItem, result[0])) });
-      } else {
-        res.render('notFound', {
-          item: '',
-          id: req.params.id,
-        });
-      }
-    }
+    res.render('item', { item: renderTemplate(matchedItem) });
   });
 });
 
---
+++ googleSpreadsheet.js
@@ -0,0 +1,44 @@
+const google = require('googleapis');
+const keys = require('./config/keys');
+
+function rowToObject(val, lab) {
+  const o = {};
+  for (let i = 0; i < lab.length; i += 1) {
+    o[lab[i]] = val[i];
+  }
+  return o;
+}
+
+function findColumn(rows, label, value) {
+  if (rows.length === 0) {
+    console.log('No data found.');
+    return;
+  }
+
+  for (let i = 0; i < rows.length; i += 1) {
+    if (rows[i][rows[0].indexOf(label)] === value) {
+      return rowToObject(rows[i], rows[0]);
+    }
+  }
+}
+
+function findEquipment(uuid, callback) {
+  const sheets = google.sheets('v4');
+  sheets.spreadsheets.values.get({
+    auth: keys.apiKey,
+    spreadsheetId: '1QHKa3vUpht7zRl_LEzl3BlUbolz3ZiL8yKHzdBL42dY',
+    range: 'Agora inventory!A:Z',
+  }, (err, response) => {
+    if (err) {
+      console.log(`The API returned an error: ${err}`);
+      return;
+    }
+    return callback(findColumn(response.values, 'uuid', uuid));
+  });
+}
+
+// export { findUuid };
+module.exports = {
+  findEquipment,
+};
+// export default findUuid;
