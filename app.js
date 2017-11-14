60c96621825dc02c383a829f3773136d5e00b6f9
Spazcool <douglas.wrightiii@gmail.com>
2017-11-14 14:08:20 +0800

    ADD: recentSent var to app.js & recent.ejs template

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const { loadDatabase, searchDatabase } = require('./googleSpreadsheet');
 
 const app = express();
 
+let recentScans = {
+  assigned:[],
+  unassigned: [],
+};
+
 app.set('view engine', 'ejs');
 
 app.use(express.static(`${__dirname}/public`));
@@ -21,16 +26,23 @@ app.get('/search', (req, res) => {
   });
 });
 
+app.get('/recent', (req, res) => {
+  res.render('recent', {data: recentScans} );
+});
+
 app.get('/:uuid', (req, res) => {
   loadDatabase((allItems) => {
     const matches = searchDatabase(req.params, allItems);
     if (matches.length === 0) {
+      recentScans.unassigned.push(req.params.uuid);
       res.render('notFound', {
         item: '',
-        id: req.params.id,
+        id: req.params.uuid,
       });
       return;
     }
+    recentScans.assigned.push([matches[0].fixture, req.params.uuid]);
+    // console.log(recentScans);
     matches.similarItems = searchDatabase({ fixture: matches[0].fixture }, allItems)
       .filter(item => item.uuid !== matches[0].uuid)
       .splice(0, 3);
@@ -39,6 +51,7 @@ app.get('/:uuid', (req, res) => {
 });
 
 app.get('/', (req, res) => {
+  // console.log(req.params);
   res.redirect('https://cryptic-woodland-88390.herokuapp.com/');
 });
 
